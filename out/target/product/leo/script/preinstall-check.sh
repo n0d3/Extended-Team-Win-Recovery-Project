8b6504ecc8e3f086b6007b5a02d19b135b0aa456
n0d3 <koko1510@gmail.com>
2013-02-09 19:16:28 +0200

    Moved option for backing up userdata to BACKUP page. Show Data or DataOnExt or DataOnNand accordingly. Update version.

--- out/target/product/leo/script/preinstall-check.sh
+++ out/target/product/leo/script/preinstall-check.sh
@@ -1,6 +1,44 @@
 #!/sbin/sh
-##############################################################
-# Simple script to check the size of the recovery's partition.
+
+if grep -Fxq "clk=" /proc/cmdline
+then
+	if [ "`which clkpartmgr`" ]; then
+		ZIP_NAME=HD2_Extended_TWRP_2.4.1.0.r1.zip
+		PART1_NAME=recovery
+		PART1_MINSIZE=6
+		############################################################################
+		# clkpartmgr is a simple cmdline tool to get TWRP and cLK		   #
+		# communicate in case we need to change the p-table layout.		   #
+		#									   #
+		# Usage: clkpartmgr [OPTIONAL:<zip-name>] <ptn-name:min-size>		   #
+		#									   #
+		# Example:								   #
+		#	  If we want to install a Rom package called 'CM10_v1.zip'	   #
+		#	  and the minimum required sizes of 'boot' and 'system'		   #
+		#	  partitions are 5MB and 260MB respectively, then 		   #
+		#	  clkpartmgr could be called when installing this Rom		   #
+		#	  to make sure that the sizes of those partitions are adequate:	   #
+		#		"clkpartmgr CM10.1_v1.zip boot:5 system:260"		   #
+		#	  In case a partition is smaller than the required value	   #
+		#	  the p-table needs to be changed, so clkpartmgr automatically	   #
+		#	  reboots the device into cLK, where the changes are applied,	   #
+		#	  and re-enters recovery to continue installing the 'CM10_v1.zip'. #
+		#									   #
+		# Notes:								   #
+		#	  *if size_of_partition >= min_size => No change to p-table.	   #
+		#	  *if the name of the zip is not given then after changing	   #
+		#	   the p-table layout, cLK will boot into recovery but		   #
+		#	   the installation of the zip will not automatically continue.	   #
+		############################################################################
+		clkpartmgr $ZIP_NAME $PART1_NAME:$PART1_MINSIZE
+		exit 0;
+	fi
+fi
+
+# Set the minimum required size of the recovery's partition
+min_size=0x005FFFFF;
+# Get the real size of the existing recovery partition
+rec_size=0x$(sed -n '/recovery/p' /proc/mtd | cut -d ' ' -f 2);
 
 # Print messages function
 OUTFD=$(ps | grep -v "grep" | grep -o -E "update_binary(.*)" | cut -d " " -f 3)
@@ -12,12 +50,6 @@ ui_print() {
 		echo "ui_print " 1>&$OUTFD
 	fi
 }
-
-# Set the minimum required size of the recovery's partition
-min_size=0x005FFFFF;
-
-# Get the real size of the existing recovery partition
-rec_size=0x$(sed -n '/recovery/p' /proc/mtd | cut -d ' ' -f 2);
 
 # Print readable sizes
 prt_rec_size=$(($rec_size / 1048576));
@@ -32,7 +64,6 @@ fi
 if [ -f /sdcard/TWRP/.version ]; then
 	rm -f "/sdcard/TWRP/*version";
 fi
-
 # Decide whether we should proceed or not
 check=$(($rec_size - $min_size));
 if [[ $check -gt 0 ]]; then
@@ -40,3 +71,4 @@ if [[ $check -gt 0 ]]; then
 else
 	exit 1;
 fi
+
