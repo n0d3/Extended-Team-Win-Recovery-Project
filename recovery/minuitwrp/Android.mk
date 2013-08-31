24214ff10fc5b43c768bf3e9c225fa07274d41c3
n0d3 <koko1510@gmail.com>
2013-08-31 13:39:01 +0300

    Remove libjpegtwrp and use CM provided libjpeg(Mainline Port)

--- recovery/minuitwrp/Android.mk
+++ recovery/minuitwrp/Android.mk
@@ -13,7 +13,8 @@ endif
 LOCAL_C_INCLUDES += \
     external/libpng \
     external/zlib \
-    system/core/include
+    system/core/include \
+    external/jpeg
 
 LOCAL_C_INCLUDES += \
     bootable/recovery/libjpegtwrp
@@ -62,8 +63,8 @@ ifneq ($(LANDSCAPE_RESOLUTION),)
   LOCAL_CFLAGS += -DTW_HAS_LANDSCAPE
 endif
 
-LOCAL_SHARED_LIBRARIES += libz libc libcutils
-LOCAL_STATIC_LIBRARIES += libpng libjpegtwrp libpixelflinger_static
+LOCAL_SHARED_LIBRARIES += libz libc libcutils libjpeg
+LOCAL_STATIC_LIBRARIES += libpng libpixelflinger_static
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE := libminuitwrp
 
